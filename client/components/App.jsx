628b137cac6d750bce3a3ded80fb86a796b9e9d6
Jake Hurley <63322006+jake-hurley@users.noreply.github.com>
2020-06-19 10:25:44 +1200

    add profile route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,6 +1,6 @@
 // DEPENDENCIES
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
 // COMPONENTS
 import Profile from './Profile'
 // DATA
@@ -8,11 +8,7 @@ import dex from '../../data.js'
 
 const App = () => {
   const dexName = dex
-  <ul>
-  { dexName.map(user => {
-    <li key={user.id}><Link to={`/user/:${name}`}>{user.name}</Link> </li>
-  }) }
-  </ul>
+
   console.log(dexName)
   return (
     <div>
@@ -21,6 +17,21 @@ const App = () => {
       </div>
       <div>
         <img src={'/images/closedpb.png'} className='closedpb' />
+        <ul>
+          { dexName.map(user => {
+            return (
+              <li key={user.id}>
+                <Link to={`/profile/${user.name}`}>{user.name}</Link>
+                <Route path={'/profile/:name'} render={() => {
+                  return (
+                    <Profile dex={dex} />
+                  )
+                }}
+                />
+              </li>
+            )
+          }) }
+        </ul>
       </div>
     </div>
   )
