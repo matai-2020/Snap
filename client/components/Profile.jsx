f9739a2ea66ecb1f6ec371f678a083272deb59f0
Rikke Simone <33711718+RikkeSimone@users.noreply.github.com>
2020-06-19 10:18:08 +1200

    added header, image and desc to profile

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 
-const Profile = () => {
-  return (
-    <div>
+import { Link } from 'react-router-dom'
 
+import dex from '../../data'
+
+const Profile = ({ match }) => {
+  const id = Number(match.params.id)
+  const details = dex.filter(item => item.id === id)[0]
+
+  return (
+    <>
+    <div>
+      <h1>{details.name}</h1>
+
+      <p>
+        <img src={details.image[0]} />
+        <h3>Your pokemon is {details.pokemon[0]}!</h3>
+        {details.description[0]}
+      </p>
     </div>
+    </>
   )
 }
 
